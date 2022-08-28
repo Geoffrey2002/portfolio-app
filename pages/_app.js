b604346f9ab7de2b12e3ce1fd7ebf6d2df20464d
Geoffrey Mwangi <gmwangi3174@gmail.com>
2022-08-28 19:51:52 +0300

    introduced aos animation on scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    Aos.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
   return (
     <>
       <Navbar />
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import About from "../components/About";
 import Contact from "../components/Contact";
 
 import Main from "../components/Main";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+// import animate on scroll
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-export default function Home() {
+const Home = () => {
+  // animate on scroll initialization
+  // Aos.init({
+  //   duration: 1800,
+  //   offset: 0,
+  // });
   return (
     <div>
       <Head>
@@ -26,4 +34,6 @@ export default function Home() {
       <Contact />
     </div>
   );
-}
+};
+
+export default Home;
